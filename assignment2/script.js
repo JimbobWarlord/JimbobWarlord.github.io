5bf6b121d4d43cea3809a22538438071c0232c58
JimbobWarlord <s4092060@student.rmit.edu.au>
2025-05-04 02:35:24 +1000

    assignment 2 figuring shit out

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,14 +1,20 @@
 //songs list
+let i = 0;
 const undertale = [
   {
     id: 0,
-    name: "Once Upon a Time",
+    name: "01. Once Upon a Time",
     src: "01.mp3",
   },
   {
     id: 1,
-    name: "Start Menu",
+    name: "02. Start Menu",
     src: "02.mp3",
+  },
+  {
+    id: 2,
+    name: "03. Your Best Friend",
+    src: "03.mp3",
   },
 ];
 
@@ -38,18 +44,75 @@ function toggleAudio() {
   }
 }
 
+//mute unmute logic
+// 1. get the right button to mute and unmute the video
+const muteUnmuteBtn = document.querySelector("#mute-unmute-btn");
+console.log(muteUnmuteBtn);
+const muteUnmuteImg = document.querySelector("#mute-unmute-img");
+console.log(muteUnmuteImg);
+
+// 2. listen to the click event
+muteUnmuteBtn.addEventListener("click", muteAudio);
+
+// 3. write the function
+function muteAudio() {
+  if (myAudio.muted) {
+    myAudio.muted = false;
+    muteUnmuteImg.src =
+      "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
+  } else {
+    myAudio.muted = true;
+    muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+  }
+}
+
+//replay logic
+// 1. get the right button to replay the video
+const replayBtn = document.querySelector("#replay-btn");
+console.log(replayBtn);
+const replayImg = document.querySelector("#replay-img");
+console.log(replayImg);
+
+// 2. listen to the click event
+replayBtn.addEventListener("click", replayAudio);
+
+// 3. write the function
+function replayAudio() {
+  myAudio.currentTime = 0;
+}
+
+//previous song logic
+const previousSongBtn = document.querySelector("#previous-song-btn");
+console.log(previousSongBtn);
+previousSongBtn.addEventListener("click", function playPreviousSong() {
+  i = i - 1;
+  if (i === -1) {
+    i = 0;
+  } else {
+    playSong(i);
+    textbox.innerHTML = undertale[i].name;
+  }
+});
+
 //next song logic
 const nextSongBtn = document.querySelector("#next-song-btn");
 console.log(nextSongBtn);
 nextSongBtn.addEventListener("click", function playNextSong() {
-  playNextSong(1);
+  i = i + 1;
+  if (i === 3) {
+    i = 2;
+  } else {
+    playSong(i);
+    textbox.innerHTML = undertale[i].name;
+  }
 });
 
-function playNextSong(no) {
+function playSong(no) {
   myAudio.src = undertale[no].src;
   console.log(myAudio.src);
   myAudio.load();
   myAudio.play();
+  myAudio.volume = 0.5;
 }
 
 // Add other functionalities here
