979cfc6347a2dd07220b3949c577731f7893131e
JimbobWarlord <s4092060@student.rmit.edu.au>
2025-06-05 21:08:05 +1000

    thing

---
+++ area 3/flip.js
@@ -0,0 +1,20 @@
+const card = document.querySelector(".card");
+console.log(card);
+
+// Flip on hover
+card.addEventListener("mouseenter", flipMe);
+card.addEventListener("mouseleave", revertBack);
+function flipMe() {
+  card.classList.add("flip");
+}
+function revertBack() {
+  card.classList.remove("flip");
+}
+
+// Flip on click
+// card.addEventListener("click", toggleFlip);
+
+// in-line
+// function toggleFlip() {
+//   card.classList.toggle("flip");
+// }
